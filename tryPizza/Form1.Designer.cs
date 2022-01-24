5c0130505e22d612b1c67a7a7b207a25f1c385bb
Matt <smithmatt53@gmail.com>
2022-01-23 23:00:33 -0600

    start git log

---
+++ tryPizza/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace tryPizza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.idTB = new System.Windows.Forms.TextBox();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.ownTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(553, 318);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(153, 54);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // idTB
+            // 
+            this.idTB.Location = new System.Drawing.Point(542, 163);
+            this.idTB.Name = "idTB";
+            this.idTB.Size = new System.Drawing.Size(184, 22);
+            this.idTB.TabIndex = 2;
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(542, 211);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(184, 22);
+            this.nameTB.TabIndex = 3;
+            // 
+            // ownTB
+            // 
+            this.ownTB.Location = new System.Drawing.Point(542, 257);
+            this.ownTB.Name = "ownTB";
+            this.ownTB.Size = new System.Drawing.Size(184, 22);
+            this.ownTB.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(490, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(490, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(490, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Own:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ownTB);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.idTB);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox idTB;
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.TextBox ownTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ tryPizza/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tryPizza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            SqlConnection cnn;
+            //connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            string connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename = C:\Users\matt_\OneDrive\Documents\pizza.mdf;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            string sql = "select * from dbo.pizza";
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            string output = "";
+
+            command = new SqlCommand(sql, cnn);
+            dataReader = command.ExecuteReader();
+
+            while(dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + " " + dataReader.GetValue(1) + " " + dataReader.GetValue(2) + "\n";
+            }
+            MessageBox.Show(output);
+            cnn.Close();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if(idTB.Text != "" && nameTB.Text != "" && ownTB.Text != "")
+            {
+                MessageBox.Show("Added!");
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
+        }
+    }
+}
